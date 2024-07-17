bb30b848f8d7733b608f63fc10b2bb9b41251d5f
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-07-17 13:05:29 -0300

    Update DBConnection.java

--- src/main/java/com/service/servlet/projeto/DB/DBConnection.java
+++ src/main/java/com/service/servlet/projeto/DB/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/biblioteca";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static Connection connection = null;
