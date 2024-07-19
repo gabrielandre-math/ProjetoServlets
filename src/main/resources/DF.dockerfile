e7269005a6bd620616a48322cc545512ec89be77
Lucas Ferreira <lucasgabrielrf.contato@gmail.com>
2024-07-19 20:03:07 -0300

    feat: banco de dados integrado ao docker

---
+++ src/main/resources/DF.dockerfile
@@ -0,0 +1,15 @@
+# Use a imagem oficial do PostgreSQL
+FROM postgres:latest
+
+# Defina as variáveis de ambiente
+ENV POSTGRES_DB=biblioteca
+ENV POSTGRES_USER=postgres
+ENV POSTGRES_PASSWORD=1234
+
+# Copie o script de inicialização e o arquivo CSV para o contêiner
+COPY init-db.sh /docker-entrypoint-initdb.d/
+COPY livros.csv /docker-entrypoint-initdb.d/
+COPY categorias.CSV /docker-entrypoint-initdb.d/
+
+# Permitir execução do script
+RUN chmod +x /docker-entrypoint-initdb.d/init-db.sh
---
+++ src/main/resources/init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(60) NOT NULL,
+    email VARCHAR(120) NOT NULL UNIQUE,
+    senha VARCHAR(60) NOT NULL,
+    isAdmin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS livros (
+    id SERIAL PRIMARY KEY,
+    isbn VARCHAR(20) NOT NULL UNIQUE,
+    nome VARCHAR(120) NOT NULL UNIQUE,
+    quantidade INT,
+    categoria_id INT,
+    image_id INT,
+    usuario_id INT,
+    CONSTRAINT categoria_id FOREIGN KEY (categoria_id) REFERENCES categoria(id),
+    CONSTRAINT image_id FOREIGN KEY (image_id) REFERENCES images(id),
+    CONSTRAINT usuario_id FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE IF NOT EXISTS images (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    data BYTEA NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categorias(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
